fdd24776932ab617013bb2d296831b2859f1c96f
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-01-19 21:36:12 +0000

    Refactor dataflow to allow easier mocking/testing

---
+++ modelling/smart_sell.py
@@ -0,0 +1,22 @@
+# Sells at statistically best time, within time limit
+# Strategy:
+#   Get a general trend using linear regressin
+#   If trend is a good fit, sell accordingly
+#   Otherwise, use more complex ML model
+
+from modelling.trends import get_gradient, parse_tw_history, upward_trend
+
+# Can use at most 29 days of transaction history from TW API
+TW_MAX_MEMORY = 29
+
+
+# now_date and sell_by_date should be integers
+# returns whether should sell now
+# rates should be sorted ascending by date, and importantly measurements should be same time apart
+def should_sell(now_date, sell_by_date, rates):
+    if now_date >= sell_by_date:
+        return True
+    memory = TW_MAX_MEMORY
+    times = list(range(len(rates)))
+    gradient, r2 = get_gradient(times, rates)
+    return (not upward_trend(gradient, r2))
